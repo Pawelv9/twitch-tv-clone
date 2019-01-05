6baf90a5870ebfc8c7e95d5196dd5b4f66e3c616
Pavel <Pawelv9@gmail.com>
2019-01-05 22:04:23 +0300

    updating Authentication

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
-    // state = { isSignedIn: null }
-
     componentDidMount = () => {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
