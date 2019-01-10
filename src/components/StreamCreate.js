3f623a3d195d1cd6c596ade7c4528677b938012a
Pavel <Pawelv9@gmail.com>
2019-01-11 02:36:13 +0300

    adding form fields

--- src/components/StreamCreate.js
+++ src/components/StreamCreate.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-  return (
-    <div>
-      StreamCreate
-    </div>
-  )
+class StreamCreate extends Component {
+  renderInput({ input, label }) {
+    // console.log(formProps)
+    return (
+      <div>
+        <label>{ label }</label>
+        <input { ...input } />
+      </div>
+    )
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title"/>
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+      </form>
+    );
+  }
 }
 
-export default StreamCreate
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
