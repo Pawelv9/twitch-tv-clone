1efc56ea569bc50f7bba62e2220dba3eecacd5b4
Pavel <Pawelv9@gmail.com>
2019-02-03 22:08:14 +0300

    updating actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
     };
 };
 
-export const createStream = formValues => async dispatch => {
-    const response = streams.post('/streams', formValues)
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = streams.post('/streams', {...formValues, userId })
 
     dispatch({ type: CREATE_STREAM, payload: response.data })
 };
