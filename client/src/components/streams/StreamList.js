38b1abf961a42c3a5ac59476b001a1f2dd991afa
Pavel <Pawelv9@gmail.com>
2019-02-03 21:55:32 +0300

    updating StreamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,17 +8,35 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
   
+  renderList() {
+    this.props.streams.map(stream => {
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned camera"/>
+        <div className="content">
+          {stream.title}
+          <div className="description">
+            {stream.description}
+          </div>
+        </div>
+      </div>
+    })
+  }
+
   render() {
     return (
       <div>
-        StreamList
+        <h2>Streams</h2>
+          <div className="ui celled list">
+            {this.renderList()}    
+          </div>
+        {StreamList}
       </div>
     );
   }
 }
 
 mapStateOfProps = () => {
-  return { streams };
+  return { streams: Object.values(state.streams) };
 }
 
-export default connect(null, { fetchStreams })(StreamList)
+export default connect(mapStateOfProps, { fetchStreams })(StreamList)
