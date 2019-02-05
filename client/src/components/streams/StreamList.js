e2d16d5752ef73ac515112302a708c034e47e2ed
Pavel <Pawelv9@gmail.com>
2019-02-05 23:38:49 +0300

    adding Edit & Delete Buttons

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,9 +8,26 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
   
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+          <button className="ui button negative">
+            Delete
+          </button>
+        </div>
+
+      )
+    }
+  }
+
   renderList() {
     this.props.streams.map(stream => {
       <div className="item" key={stream.id}>
+        {this.renderAdmin(stream)}
         <i className="large middle aligned camera"/>
         <div className="content">
           {stream.title}
@@ -36,7 +53,10 @@ class StreamList extends Component {
 }
 
 mapStateOfProps = () => {
-  return { streams: Object.values(state.streams) };
+  return { 
+    streams: Object.values(state.streams),
+    userId: state.auth.userId
+    };
 }
 
 export default connect(mapStateOfProps, { fetchStreams })(StreamList)
