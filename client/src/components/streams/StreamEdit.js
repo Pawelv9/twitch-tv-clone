940a3fd8c9db94a7512817e3b0f499782813166c
Pavel <Pawelv9@gmail.com>
2019-02-20 23:55:07 +0300

    fetching stream for StreamEdit

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-  return (
-    <div>
-      StreamEdit
-    </div>
-  )
+
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+  
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>
+    
+  }  
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps)
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit)
