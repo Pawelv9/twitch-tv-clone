24400b37c8ad3aa9d48016eea07fd29dd98d13af
Pavel <Pawelv9@gmail.com>
2019-02-24 00:18:08 +0300

    updating a Modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Modal from '../Modal';
+import history from '../../history';
 
 
 const StreamDelete = () => {
+  const actions = (
+    <Fragment>
+      <button className="ui primary button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </Fragment>
+    )
+  
   return (
     <div>
       StreamDelete
-      <Modal/>
+      <Modal title="Delete Stream" content="Your Stream will be deleted" actions={actions} onDismiss={() => history.push('/')}/>
     </div>
   );
 };
