52320d7eaa83449c71cedab6402728b2b51bfd69
Pavel <Pawelv9@gmail.com>
2019-02-24 22:55:51 +0300

    adding Video Player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 
 class StreamShow extends Component {
   
+  constructor(props) {
+   super(props)
+   
+   this.videoRef = React.createRef();
+  }
+
   componentDidMount = () => {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  
+  
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player=flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   
   
@@ -19,6 +52,7 @@ class StreamShow extends Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{width: '100%'}} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
